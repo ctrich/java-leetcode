559a8c76a7217e050659502d95e56f69d3703e01
ctrich <ctrich86@yahoo.com>
2023-10-14 10:50:47 -0700

    comments

--- Solution.java
+++ Solution.java
@@ -2,6 +2,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
+    //Takes an integer array nums, return true if any value appears at least
+    // twice in the array, and return false if every element is distinct.
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> setNums = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
